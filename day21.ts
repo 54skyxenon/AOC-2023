e6ccf6bc7a98e9e49c1bb4aad9879b1019fd53aa
Brandon Liang <54skyxenon@gmail.com>
2024-01-05 23:54:22 -0800

    Day 21

--- day21.ts
+++ day21.ts
@@ -7,7 +7,16 @@ const lines: string[] = _.reject(
   _.isEmpty
 );
 
-const solveBfs = (limit: number) => {
+const getStartingPosition = (): [number, number] =>
+  _.find(
+    _.product(_.range(0, lines.length), _.range(0, lines[0].length)),
+    ([row, col]) => lines[row][col] === "S"
+  )! as [number, number];
+
+/**
+ * Naive solution of just enumerating every position `steps` amount away from a certain start point.
+ */
+const solveBfs = (startingRow: number, startingCol: number, steps: number) => {
   const height = lines.length;
   const width = lines[0].length;
 
@@ -19,13 +28,8 @@ const solveBfs = (limit: number) => {
   ];
 
   const visited: number[][][] = _.times(height, () =>
-    _.times(width, () => _.times(limit + 1, _.constant(0)))
+    _.times(width, () => _.times(steps + 1, _.constant(0)))
   );
-
-  const [startingRow, startingCol] = _.find(
-    _.product(_.range(0, height), _.range(0, width)),
-    ([row, col]) => lines[row][col] === "S"
-  )!;
 
   visited[startingRow][startingCol][0] = 1;
   const queue: [number, number, number][] = [[startingRow, startingCol, 0]];
@@ -41,7 +45,7 @@ const solveBfs = (limit: number) => {
         nc < 0 ||
         nc >= width ||
         lines[nr][nc] === "#" ||
-        depth + 1 > limit ||
+        depth + 1 > steps ||
         visited[nr][nc][depth + 1] === 1
       ) {
         return;
@@ -54,63 +58,74 @@ const solveBfs = (limit: number) => {
 
   return _.sum(
     _.product(_.range(0, height), _.range(0, width)).map(
-      ([row, col]) => visited[row][col][limit]
+      ([row, col]) => visited[row][col][steps]
     )
   );
 };
 
-console.log(solveBfs(64));
-
-const advanceStep = (captured: number[], goodMods: Set<number>): number[] => {
-  const height = lines.length;
-  const width = lines[0].length;
-  const totalCells = height * width;
-  const adjacent: number[] = [-1, 1, -width, width];
-  const newCaptured = new Set<number>();
-
-  for (const cell of captured) {
-    adjacent.forEach((adj) => {
-      const newCell = (cell + adj + totalCells) % totalCells;
-
-      if (goodMods.has(newCell)) {
-        newCaptured.add(newCell);
-      }
-    });
-  }
-
-  return Array.from(newCaptured);
-};
+console.log(solveBfs(...getStartingPosition(), 64));
 
 /**
- * Optimization ideas: map each cell to a 2d cell and create a mod system from that!
+ * Gave up on this one and translated HyperNeutrino's solution with my own annotations:
+ * https://www.youtube.com/watch?v=9UOMZSL0JTg
+ *
+ * ^ I highly recommend watching, it's a great explanation!
+ *
+ * Assumptions:
+ * - The grid is always a square
+ * - Grid width of 131 yields divmod(26501365, 131) => (202300, 65)
  */
-const solveMod = (limit: number) => {
-  const height = lines.length;
-  const width = lines[0].length;
-  const flattened = Array.from(lines.join(""));
+const solveEfficiently = (steps: number): number => {
+  const size = lines.length;
+  const gridWidth = Math.floor(steps / size) - 1;
+  const [sr, sc] = getStartingPosition();
 
-  const goodMods: Set<number> = new Set();
+  // Number of complete grids of odd/even Manhattan distance from the start
+  const odd = (Math.floor(gridWidth / 2) * 2 + 1) ** 2;
+  const even = (Math.floor((gridWidth + 1) / 2) * 2) ** 2;
 
-  const [startingRow, startingCol] = _.find(
-    _.product(_.range(0, height), _.range(0, width)),
-    ([row, col]) => lines[row][col] === "S"
-  )!;
+  // Visited cells in a single complete grid of odd/even Manhattan distance from the start
+  const oddPoints = solveBfs(sr, sc, size * 2 + 1);
+  const evenPoints = solveBfs(sr, sc, size * 2);
 
-  flattened.forEach((c, i) => {
-    if (c !== "#") {
-      goodMods.add(i);
-    }
-  });
+  // Visited cells in 4 edge grids of the visited range
+  // ! There's a direct path from the elf to the corners, so we can do this
+  const cornerSteps = size - 1;
+  const cornerT = solveBfs(size - 1, sc, cornerSteps);
+  const cornerR = solveBfs(sr, 0, cornerSteps);
+  const cornerB = solveBfs(0, sc, cornerSteps);
+  const cornerL = solveBfs(sr, size - 1, cornerSteps);
 
-  let captured = [startingRow * width + startingCol];
+  // Where the elf starts in the non-direct outermost layer grids
+  const entryPoints = [
+    [size - 1, 0],
+    [size - 1, size - 1],
+    [0, 0],
+    [0, size - 1],
+  ];
 
-  for (let i = 0; i < limit; i++) {
-    captured = advanceStep(captured, goodMods);
-  }
+  // Visited cells within the smaller half cut in the non-direct outermost layer grids (besides the 4 corner grids)
+  const smallSteps = Math.floor(size / 2) - 1;
+  const [smallTr, smallTl, smallBr, smallBl] = entryPoints.map(([r, c]) =>
+    solveBfs(r, c, smallSteps)
+  );
 
-  console.log(captured);
+  // Visited cells within the larger half cut in the non-direct outermost layer grids (besides the 4 corner grids)
+  const largeSteps = Math.floor((size * 3) / 2) - 1;
+  const [largeTr, largeTl, largeBr, largeBl] = entryPoints.map(([r, c]) =>
+    solveBfs(r, c, largeSteps)
+  );
 
-  return captured;
+  return (
+    odd * oddPoints +
+    even * evenPoints +
+    cornerT +
+    cornerR +
+    cornerB +
+    cornerL +
+    (gridWidth + 1) * (smallTr + smallTl + smallBr + smallBl) +
+    gridWidth * (largeTr + largeTl + largeBr + largeBl)
+  );
 };
 
-solveMod(10);
+console.log(solveEfficiently(26501365));
