c95dde7b1f9dde3fb954cb10693cf6f2e39f71de
Brandon Liang <54skyxenon@gmail.com>
2024-01-07 00:43:24 -0800

    Day 23

--- day23.ts
+++ day23.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { Dictionary } from "lodash";
 import "lodash.product";
 import fs from "fs";
 
@@ -15,157 +15,151 @@ const startCol = lines[0].split("").findIndex((c) => c === ".");
 const endRow = height - 1;
 const endCol = lines[endRow].split("").findIndex((c) => c === ".");
 
-const adjacent = [
+const adjacent: [number, number][] = [
   [-1, 0],
   [1, 0],
   [0, -1],
   [0, 1],
 ];
 
-const policy: Record<string, number[]> = {
-  "^": [-1, 0],
-  ">": [0, 1],
-  v: [1, 0],
-  "<": [0, -1],
-};
+const policy: Dictionary<number[]> = _.zipObject(
+  ["^", "v", "<", ">"],
+  adjacent
+);
 
-const dfs = (row: number, col: number, visited: Set<number>): number => {
-  if (row === endRow && col === endCol) {
-    return 0;
-  }
-
-  let opt = 0;
-
-  adjacent.forEach(([dr, dc]) => {
-    const neighborId = (row + dr) * width + (col + dc);
-    const required = policy[lines[row][col]];
-
-    if (
-      row + dr < 0 ||
-      row + dr >= height ||
-      col + dc < 0 ||
-      col + dc >= width ||
-      (required && !_.isEqual([dr, dc], required)) ||
-      visited.has(neighborId) ||
-      lines[row + dr][col + dc] === "#"
-    ) {
-      return;
+const part1 = (): number => {
+  // Represent visited cells as a single number to avoid set lookup comparison ambiguity
+  const dfs = (row: number, col: number, visited: Set<number>): number => {
+    if (row === endRow && col === endCol) {
+      return 0;
     }
 
-    visited.add(neighborId);
-    opt = Math.max(opt, 1 + dfs(row + dr, col + dc, visited));
-    visited.delete(neighborId);
-  });
-
-  return opt;
-};
-
-interface AdjListNode {
-  v: number;
-  weight: number;
-}
-
-class Graph {
-  V: number;
-  adj: AdjListNode[][];
-
-  constructor(V: number) {
-    this.V = V;
-    this.adj = _.times(V, () => []);
-  }
-
-  topologicalSortUtil(v: number, visited: boolean[], stack: number[]) {
-    visited[v] = true;
-
-    for (let j in this.adj[v]) {
-      let i = this.adj[v][j];
-      let node = i;
-      if (!visited[node.v]) this.topologicalSortUtil(node.v, visited, stack);
-    }
-
-    stack.push(v);
-  }
-
-  addEdge(u: number, v: number, weight: number) {
-    this.adj[u].push({ v, weight });
-  }
-
-  longestPath(s: number): number[] {
-    const stack: number[] = [];
-    const visited = _.times(this.V, _.constant(false));
-
-    for (let i = 0; i < this.V; i++) {
-      if (!visited[i]) {
-        this.topologicalSortUtil(i, visited, stack);
-      }
-    }
-
-    const dist = _.times(this.V, _.constant(Number.MIN_VALUE));
-    dist[s] = 0;
-
-    while (stack.length > 0) {
-      let u = stack.pop()!;
-
-      if (dist[u] != Number.MIN_VALUE) {
-        this.adj[u].forEach(({ v, weight }) => {
-          if (dist[v] < dist[u] + weight) {
-            dist[v] = dist[u] + weight;
-          }
-        });
-      }
-    }
-
-    return dist;
-  }
-}
-
-/**
- * @link https://www.geeksforgeeks.org/find-longest-path-directed-acyclic-graph/
- */
-const dfsEfficient = (startRow: number, startCol: number): number => {
-  const graph = new Graph(height * width);
-
-  // Stack based DFS
-  const stack: [number, number][] = [[startRow, startCol]];
-  const visited = new Set<number>([startRow * width + startCol]);
-
-  while (stack.length > 0) {
-    const [r, c] = stack.pop()!;
-    const thisId = r * width + c;
+    let ans = 0;
 
     adjacent.forEach(([dr, dc]) => {
-      const newRow = r + dr;
-      const newCol = c + dc;
-      const neighborId = newRow * width + newCol;
+      const neighborId = (row + dr) * width + (col + dc);
+      const required = policy[lines[row][col]];
 
       if (
-        newRow < 0 ||
-        newRow >= height ||
-        newCol < 0 ||
-        newCol >= width ||
-        lines[newRow][newCol] === "#"
+        row + dr < 0 ||
+        row + dr >= height ||
+        col + dc < 0 ||
+        col + dc >= width ||
+        (required && !_.isEqual([dr, dc], required)) ||
+        visited.has(neighborId) ||
+        lines[row + dr][col + dc] === "#"
       ) {
         return;
       }
 
-      if (!visited.has(neighborId)) {
-        stack.push([newRow, newCol]);
-        visited.add(neighborId);
-      }
-
-      graph.addEdge(thisId, neighborId, 1);
+      visited.add(neighborId);
+      ans = Math.max(ans, 1 + dfs(row + dr, col + dc, visited));
+      visited.delete(neighborId);
     });
-  }
 
-  return graph.longestPath(startRow * width + startCol)[
-    endRow * width + endCol
-  ];
+    return ans;
+  };
+
+  return dfs(
+    startRow,
+    startCol,
+    new Set<number>([startRow * width + startCol])
+  );
+};
+
+const part2 = (): number => {
+  // Start, end, or >= 3 adjacent neighbors
+  const crossroads = _.product(_.range(height), _.range(width)).filter(
+    ([r, c]) =>
+      lines[r][c] === "." &&
+      ((r === startRow && c === startCol) ||
+        (r === endRow && c === endCol) ||
+        adjacent.filter(
+          ([dr, dc]) =>
+            r + dr >= 0 &&
+            r + dr < height &&
+            c + dc >= 0 &&
+            c + dc < width &&
+            lines[r + dr][c + dc] !== "#"
+        ).length >= 3)
+  );
+
+  const compressedGraph: [number, number, number][][] = _.times(
+    crossroads.length,
+    () => []
+  );
+
+  // BFS until you hit another crossroads
+  crossroads.forEach(([r, c], i) => {
+    const visited = new Set<number>([r * width + c]);
+    const queue = [[r, c, 0]];
+
+    while (queue.length > 0) {
+      const [row, col, depth] = queue.shift()!;
+
+      adjacent.forEach(([dr, dc]) => {
+        const nr = row + dr;
+        const nc = col + dc;
+        const neighborId = nr * width + nc;
+
+        if (
+          nr < 0 ||
+          nr >= height ||
+          nc < 0 ||
+          nc >= width ||
+          lines[nr][nc] === "#" ||
+          visited.has(neighborId)
+        ) {
+          return;
+        }
+
+        if (
+          crossroads.some(([otherR, otherC]) => otherR === nr && otherC === nc)
+        ) {
+          compressedGraph[i].push([nr, nc, depth + 1]);
+          return;
+        }
+
+        visited.add(neighborId);
+        queue.push([nr, nc, depth + 1]);
+      });
+    }
+  });
+
+  // Represent visited cells as a single number to avoid set lookup comparison ambiguity
+  const dfs = (r: number, c: number, visited: Set<number>): number => {
+    if (r === endRow && c === endCol) {
+      return 0;
+    }
+
+    // ! Number.MIN_VALUE or 0 may overcount if all child paths fail, -Infinity is safer
+    let ans = -Infinity;
+
+    const crossroadsIndex = crossroads.findIndex(
+      ([otherR, otherC]) => otherR === r && otherC === c
+    )!;
+
+    compressedGraph[crossroadsIndex].forEach(([nr, nc, cost]) => {
+      const neighborId = nr * width + nc;
+      if (!visited.has(neighborId)) {
+        visited.add(neighborId);
+        ans = Math.max(ans, cost + dfs(nr, nc, visited));
+        visited.delete(neighborId);
+      }
+    });
+
+    return ans;
+  };
+
+  return dfs(
+    startRow,
+    startCol,
+    new Set<number>([startRow * width + startCol])
+  );
 };
 
 // Part 1
-console.log(
-  dfs(startRow, startCol, new Set<number>([startRow * width + startCol]))
-);
+console.log(part1());
 
 // Part 2
-console.log(dfsEfficient(startRow, startCol));
+console.log(part2());
